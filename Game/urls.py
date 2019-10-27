180bc215676b3c2330ca09c99d8c2eac3cc03fbe
Wine <wodwine@hotmail.com>
2019-10-27 12:32:45 +0700

    Add login

--- Game/urls.py
+++ Game/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'Game'
 urlpatterns = [
+    path('home/',views.home),
+    path('login/',views.login),
+    path('login/host',views.login_host),
+    path('login/guest',views.login_guest),
     path('test/', views.hi),
     path('play/<str:quiz_name>/<int:question_number>/',views.play_quiz,name = "play_quiz"),
 ]
--- Game/views.py
+++ Game/views.py
@@ -9,4 +9,15 @@ def play_quiz(request, quiz_name, question_number):
     quiz = get_object_or_404(Quiz, quizz_name = quiz_name)
     q = {'question':quiz.question_set.all()[question_number-1]}
     return render(request,'Game/question_play.html',q)
-    
+
+def home(request):
+    return HttpResponse("This is home")
+
+def login(request):
+    return render(request,'Login/login.html')
+
+def login_host(request):
+    return render(request,'Login/loginhost.html')
+
+def login_guest(request):
+    return render(request,'Login/loginguest.html')
