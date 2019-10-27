8be3db70a4a992e18231f368818b86a899d29666
Wine <wodwine@hotmail.com>
2019-10-27 12:49:43 +0700

    To PP

--- Game/urls.py
+++ Game/urls.py
@@ -5,7 +5,7 @@ app_name = 'Game'
 urlpatterns = [
     path('home/',views.home),
     path('login/',views.login),
-    path('login/host',views.login_host),
+    path('login/host',views.login_host,name = "login_host"),
     path('login/guest',views.login_guest),
     path('test/', views.hi),
     path('play/<str:quiz_name>/<int:question_number>/',views.play_quiz,name = "play_quiz"),
