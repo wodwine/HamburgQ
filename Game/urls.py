f803ab8215ff6a98a7a199b0814882f70eb937dd
Thananan <minecraftskinman@gmail.com>
2019-10-27 10:21:42 +0700

    base question page

--- Game/urls.py
+++ Game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'Game'
 urlpatterns = [
     path('test/', views.hi),
     path('play/<str:quiz_name>/<int:question_number>/',views.play_quiz,name = "play_quiz"),
