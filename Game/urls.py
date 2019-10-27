cdf932211b82b6d783707384364f6df7e7b06435
Wine <wodwine@hotmail.com>
2019-10-27 13:20:16 +0700

    Add link button

--- Game/urls.py
+++ Game/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'Game'
 urlpatterns = [
     path('home/',views.home),
-    path('login/',views.login),
+    path('login/',views.login,name = "login"),
     path('login/host',views.login_host,name = "login_host"),
-    path('login/guest',views.login_guest),
+    path('login/guest',views.login_guest,name = 'login_guest'),
     path('test/', views.hi),
     path('play/<str:quiz_name>/<int:question_number>/',views.play_quiz,name = "play_quiz"),
 ]
