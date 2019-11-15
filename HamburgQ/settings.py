676ded41bd4b4e190d0aeb2ed2adf8904d428bf7
Wine <wodwine@hotmail.com>
2019-11-15 16:33:03 +0700

    Delete heroku2

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
