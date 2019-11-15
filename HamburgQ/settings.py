1c035701d7b2ec5be64a2999ff4c5943d5a7f62a
Wine <wodwine@hotmail.com>
2019-11-15 13:20:10 +0700

    Add heroku

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
