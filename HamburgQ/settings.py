d1bd87ea1c8f3538d1ce6678adb3e55a04e43a2f
Wine <wodwine@hotmail.com>
2019-11-15 15:25:15 +0700

    Add setting.py

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -98,13 +98,22 @@ WSGI_APPLICATION = 'HamburgQ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
-    # 'default': {
-    #     'ENGINE': config('DATABASE_ENGINE',default="django.db.backends.sqlite3"),
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-}
+if DEBUG==False:
+    DATABASES = {
+        'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
+        # 'default': {
+        #     'ENGINE': config('DATABASE_ENGINE',default="django.db.backends.sqlite3"),
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # }
+    }
+else:
+    DATABASES = {
+        # 'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': {
+            'ENGINE': config('DATABASE_ENGINE',default="django.db.backends.sqlite3"),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
