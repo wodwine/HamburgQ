57db46ef547e0187af4bd54a2af0c8a05255dab3
Wine <wodwine@hotmail.com>
2019-11-15 09:59:02 +0700

    Move script to statics quiz.js

--- Game/static/Game/js/quiz.js
+++ Game/static/Game/js/quiz.js
@@ -1,1 +1,15 @@
-console.log('hi');
+window.setTimeout(function () {
+    //check all radio
+    var radios = document.getElementsByTagName('input');
+    var no_checked = true;
+    for (var i = 0; i < radios.length; i++) {
+    if (radios[i].type === 'radio' && radios[i].checked) {
+        no_checked = false;
+        }
+    }
+    if(no_checked){
+        document.getElementById('radio0').checked = true;
+        document.getElementById('radio0').value = currentPlayerId+"$$LATE$$"+roomId;
+    }
+    document.answer.submit();
+}, roomTime * 1000);
