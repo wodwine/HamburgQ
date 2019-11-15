dc3dbd12a3403126f20472aee7e2f3772de5148b
Wine <wodwine@hotmail.com>
2019-11-15 15:18:09 +0700

    Change DATABASE

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
