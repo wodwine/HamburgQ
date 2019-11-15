23eb3919e5b17db12831738888acf2b3f16a88e1
Wine <wodwine@hotmail.com>
2019-11-15 14:06:16 +0700

    Change ALLOW_HOSTS

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY",default = 'sefwejgmerijgmerohkb')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool,default = False)
 
-ALLOWED_HOSTS = config('ALLOW_HOST')
+ALLOWED_HOSTS = config('ALLOW_HOSTS',cast = list,default = ["*"])
 SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '759857441239-fa9go0qliajblplqp7v5okij3njq10gr.apps.googleusercontent.com'
