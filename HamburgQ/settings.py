3ea9626a4eb35001e7e5d498efc2c30a4799b9d2
Wine <wodwine@hotmail.com>
2019-11-15 15:19:56 +0700

    Change DATABASE

--- HamburgQ/settings.py
+++ HamburgQ/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -98,12 +98,13 @@ WSGI_APPLICATION = 'HamburgQ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DATABASE_ENGINE',default="django.db.backends.sqlite3"),
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
+    # 'default': {
+    #     'ENGINE': config('DATABASE_ENGINE',default="django.db.backends.sqlite3"),
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+}
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
