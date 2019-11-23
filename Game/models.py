434c7ed87df1eadc097a750056054157165f5531
Thananan <minecraftskinman@gmail.com>
2019-11-23 13:49:20 +0700

    commit bofore merge

--- Game/models.py
+++ Game/models.py
@@ -20,7 +20,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    answer = models.BooleanField(default=False)
+    answer = models.BooleanField(default=False     )
 
     def __str__(self):
         return self.choice_text
